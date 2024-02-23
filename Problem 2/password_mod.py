ea8cfad7b558d6e6eb75a3dca1be01a10b2bb8ee
Emily Burke <erb9078@utulsa.edu>
2024-02-23 16:14:36 -0600

    almost done with problem 3

--- Problem 2/password_mod.py
+++ Problem 2/password_mod.py
@@ -1,8 +1,8 @@
 """
 Complete the following python code to print the password entered by the user with the modifications described in the readme
 
-Name:
-Lab Time:
+Name: Emily Burke
+Lab Time: Friday at 3
 """
 
 def password_mod():
--- Problem 3/inc_5.py
+++ Problem 3/inc_5.py
@@ -1,13 +1,19 @@
 """
 Complete the following python code to print all numbers between the two values incrementing by 5 from the initial value to the final value. The initial value and final value are entered by the user. If the final value is less than the initial value, print "Second integer can't be less than the first.
 
-Name:
-Lab Time:
+Name: Emily Burke
+Lab Time: Friday at 3
 """
 
 def inc_5():
     # Write your code here
-    
+    num1 = int(input())
+    num2 = int(input())
+    if num2<num1 :
+        print("Second integer can't be less than the first.")
+    if num1 <= num2:
+        
+
 
 
 
